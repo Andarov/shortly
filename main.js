3b0231fe0961ad422e6c46a01cc9efc2cd6a8814
Andarov <bekturandarob678@gmail.com>
2022-05-20 11:25:37 +0500

    Add timeout nad change text content and background-color when click btn-results

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 let elForm = document.querySelector(".js-form");
 let elResults = document.querySelector(".results");
+let elBtnResults = document.querySelector(".js-btn-results");
 
-elForm.addEventListener ("submit", function (evt) {
-    evt.preventDefault();
+if (elForm) {
+    elForm.addEventListener ("submit", function (evt) {
+        evt.preventDefault();
+    
+        elResults.classList.add("results--open");
+    });
+}
 
-    elResults.classList.add("results--open");
-});
+if (elResults) {
+    elResults.addEventListener("click", function (evt) {
+        if (evt.target.matches (".js-btn-results")) {
+            evt.target.textContent = "Copied";
+
+            evt.target.classList.add("btn-results-dark");
+
+            setTimeout(function () {
+                evt.target.textContent = "Copy";
+                evt.target.classList.remove("btn-results-dark");
+            },1000)
+        }
+    })
+}
+
 
 
